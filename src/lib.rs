05f1acf81eb682b5d3fb6a1b0a53e67c3d957c36
Carl Fredrik Samson <cf@samson.no>
2020-07-21 02:18:02 +0200

    Added method for initializing the sensor with a pre-initialized channel
    Multiplexers will change the addressing scheme for the sensor so the
    convenient init method with searches in the predefined range between
    `0x36..=0x39` will not work.
    
    I added a method which take an `rppal::i2c::I2c` channel and does the
    necessary checks on that to initialize the sensor.
    
    This also turned out to need some refactoring of the other methods to
    avoid duplicating the initialization code of which it's preferable
    to only have one place that's written.
    
    I also added some new errors so that we don't leak errors from `rppal`
    in the "expected" cases.

--- src/lib.rs
+++ src/lib.rs
@@ -54,13 +54,45 @@ pub struct SoilSensor<D: DelayUs<u16>> {
 }
 
 impl<D: DelayUs<u16>> SoilSensor<D> {
+    /// Initializes the sensor
     pub fn init(mut delay: D) -> Result<Self> {
-        let channel = init::channel_init(&mut delay)?;
+        let mut channel = i2c::I2c::new()?;
+        let mut hw_found: bool = false;
+
+        for adr in SENSOR_START_ADDR..=SENSOR_END_ADDR {
+            channel.set_slave_address(adr)?;
+            debug!("Connecting to adr: {:#X}", adr);
+
+            match init::channel_init(&mut delay, &mut channel) {
+                Ok(()) => {
+                    hw_found = true;
+                    break;
+                }
+                Err(SoilSensErr::HardwareMismatch(..)) => continue,
+                Err(SoilSensErr::InvalidSlaveAddress(..)) => continue,
+                Err(e) => return Err(e),
+            }
+        }
+        if !hw_found {
+            return Err(SoilSensErr::HwNotFound);
+        }
 
         Ok(SoilSensor { channel, delay })
     }
 
-    /// Reads the temperature off the soil sensor. The temperature is in Celcius.
+    /// Creates an instance from a pre-set channel. Useful if you want to communicate with
+    /// the sensors through a multiplexer or if your sensor for some reason is not in the standard
+    /// address range or needs some additional initialization before communicating with the sensor.
+    ///
+    /// This method still initializes the sensor and performs the necessary checks.
+    pub fn init_with_channel(mut delay: D, channel: I2c) -> Result<Self> {
+        // Initialize sensor.
+        let mut channel = channel;
+        init::channel_init(&mut delay, &mut channel)?;
+        Ok(SoilSensor { channel, delay })
+    }
+
+    /// Reads the temperature off the soil sensor. The temperature is in Celsius.
     ///
     /// The temperature sensor is not high precision but should be indicate the temperature
     /// +/- 2 degrees.
@@ -74,8 +106,8 @@ impl<D: DelayUs<u16>> SoilSensor<D> {
         let tmp_val = i32::from_be_bytes(buffer) as f32;
 
         // See: https://github.com/adafruit/Adafruit_Seesaw/blob/8728936a5d1a0a7bf2887a82adb0828b70556a45/Adafruit_seesaw.cpp#L664
-        let temp_celcius = (1.0 / (1u32 << 16) as f32) * tmp_val;
-        Ok(temp_celcius)
+        let temp_celsius = (1.0 / (1u32 << 16) as f32) * tmp_val;
+        Ok(temp_celsius)
     }
 
     /// Read the value of the moisture sensor
@@ -95,7 +127,7 @@ impl<D: DelayUs<u16>> SoilSensor<D> {
             self.delay.delay_us(1000);
             // NB! Setting this to 1000 (like in the C library) errors.
             if let Err(e) = self.read(l_reg, h_reg, &mut buff, 5000) {
-                debug!("Error reading capacitance: {}", e);
+                debug!("Error reading capacitance: {}. Retry: {}", e, retry_counter + 1);
                 retry_counter += 1;
                 continue;
             }
@@ -117,7 +149,7 @@ impl<D: DelayUs<u16>> SoilSensor<D> {
         self.channel.write(&[reg_low, reg_high])?;
         self.delay.delay_us(delay_us);
         self.channel.read(buff)?;
-        debug!("{:?}", buff);
+        debug!("Received: {:?}", buff);
         Ok(())
     }
 }
@@ -126,48 +158,41 @@ mod init {
     use super::*;
 
     /// Initialize the channel
-    pub fn channel_init<D: DelayUs<u16>>(delay: &mut D) -> Result<I2c> {
-        let mut chan = i2c::I2c::new()?;
-
-        for adr in SENSOR_START_ADDR..=SENSOR_END_ADDR {
-            debug!("Connecting to adr: {:#X}", adr);
-
-            match try_read_chan(&mut chan, adr, delay) {
-                Ok(resp) => {
-                    debug!("Found device with HW id: {}", resp);
-                    if resp != regs::SEESAW_HW_ID_CODE {
-                        return Err(SoilSensErr::from(I2CError::InvalidSlaveAddress(adr)));
-                    } else {
-                        debug!("HW ID match: exp {}, got: {}", resp, regs::SEESAW_HW_ID_CODE);
-                        return Ok(chan);
-                    }
-                }
-                Err(SoilSensErr::I2C {
-                    source: I2CError::InvalidSlaveAddress(adr),
-                }) => {
-                    debug!("Invalid address: {}", adr);
-                    continue;
-                }
-                Err(e) => {
-                    debug!("Unexpected err: {}", e);
-                    continue;
+    pub fn channel_init<D: DelayUs<u16>>(delay: &mut D, chan: &mut I2c) -> Result<()> {
+        match try_read_chan(chan, delay) {
+            Ok(resp) => {
+                debug!("Found device with HW id: {}", resp);
+                if resp != regs::SEESAW_HW_ID_CODE {
+                    return Err(SoilSensErr::HardwareMismatch(regs::SEESAW_HW_ID_CODE, resp));
+                } else {
+                    debug!("HW ID match: exp {}, got: {}", resp, regs::SEESAW_HW_ID_CODE);
+                    return Ok(());
                 }
             }
-        }
 
-        Err(SoilSensErr::HwNotFound)
+            Err(SoilSensErr::I2C {
+                source: I2CError::InvalidSlaveAddress(adr),
+            }) => {
+                debug!("Invalid address: {}", adr);
+                return Err(SoilSensErr::InvalidSlaveAddress(adr));
+            }
+
+            Err(e) => {
+                debug!("Unexpected err: {}", e);
+                return Err(e);
+            }
+        }
     }
 
-    // The fallible initalization code which we'll call for the entire valid address range
-    fn try_read_chan<D: DelayUs<u16>>(chan: &mut I2c, adr: u16, delay: &mut D) -> Result<u8> {
+    // The fallible initialization code which we'll call for the entire valid address range
+    fn try_read_chan<D: DelayUs<u16>>(chan: &mut I2c, delay: &mut D) -> Result<u8> {
         let reg_high = regs::base::SEESAW_STATUS_BASE;
         let reg_low = regs::func::SEESAW_STATUS_HW_ID;
-        chan.set_slave_address(adr)?;
         chan.write(&[reg_high, reg_low])?;
         let mut buffer = [0];
         delay.delay_us(STD_PROCESSING_DELAY_MICROS);
 
-        let numbytes = chan.read(&mut buffer)?;
+        chan.read(&mut buffer)?;
         debug!("Got: {:?}", buffer);
         Ok(buffer[0])
     }
@@ -179,6 +204,10 @@ pub enum SoilSensErr {
     MoistureReadErr,
     #[error("Couldn't connect to the sensor.")]
     HwNotFound,
+    #[error("Invalid Hardware ID. Expected {0}, got {1}")]
+    HardwareMismatch(u8, u8),
+    #[error("invalid slave address: {0:#X}")]
+    InvalidSlaveAddress(u16),
     #[error("I2C connection error. {source}")]
     I2C {
         #[from]
